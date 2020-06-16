3ff1f978cfcb8f830eb3c2240f6c31662eeb1f8c
PorridgePi <www.kunbao2006@gmail.com>
2020-06-16 16:28:37 +0800

    done for the day yay lol

--- pascalsTriangle.py
+++ pascalsTriangle.py
@@ -1,5 +1,16 @@
-def f(a):
-    if a == 0:
-        return 1:
+def fact(n):
+    if n == 0:
+        return 1
     else:
-        return 
+        return n * fact(n-1)
+
+def pascals_triangle(r):
+    for n in range(r):
+        a = fact(n)
+        b = fact(r)
+        c = n-r
+        print(c)
+        d = fact(c)
+        print(a,b,d)
+
+pascals_triangle(4)
--- phoneNum.py
+++ phoneNum.py
@@ -25,4 +25,4 @@ while True:
     conti=input('Enter another phone number? (y/n): ')
     if conti == 'n':
         break
-    
+        
