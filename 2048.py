fa48a99b22eadf5fdddc502f4a4d69fc34f60ab0
PorridgePi <www.kunbao2006@gmail.com>
2020-06-27 13:46:19 +0800

    started on tetris

---
+++ 2048.py
@@ -0,0 +1,23 @@
+def printBoard(state):
+    for i in state:
+        for item in i:
+            print(item, end='\t')
+        print()
+
+
+def move(state, direction):
+    pass
+
+
+def main():
+    pass
+
+
+state = [
+    [2, 0, 0, 2],
+    [4, 16, 8, 2],
+    [2, 64, 32, 4],
+    [1024, 1024, 64, 0]
+]
+
+printBoard(state)
