93c1542bbe49d2003d980e7434b7ed0fa3ecaf37
PorridgePi <www.kunbao2006@gmail.com>
2020-06-27 16:59:55 +0800

    finished movement logics for 2048

--- 2048.py
+++ 2048.py
@@ -1,25 +1,63 @@
+def transpose(state):
+    newState = []
+    for i in range(len(state[0])):
+        newState.append([])
+        for j in range(len(state)):
+            newState[i].append(state[j][i])
+    return newState
 
-def transpose(matrix):
-    result = []
-    for i in range(len(matrix[0])):
-        new.append([])
-        for j in range(len(matrix)):
-            new[i].append(matrix[j][i])
-    return result
+
+def reverse(state):
+    newState = []
+    for i in range(len(state)):
+        newState.append([])
+        for j in range(len(state[0])):
+            newState[i].append(state[i][len(state[0])-j-1])
+    return newState
+
 
 def printBoard(state):
     for i in state:
         for item in i:
             print(item, end='\t')
         print()
+    print()
+
+
+
+def moveLeft(state):
+    newState = [[], [], [], []]
+    for l in range(len(state)):
+        j = [a for a in state[l] if a != 0]
+        prevNum = None
+        for i in range(len(j)):
+            if j[i] == prevNum:
+                newState[l].append(j[i]*2)
+                prevNum = None
+            else:
+                if prevNum != None:
+                    newState[l].append(prevNum)
+                prevNum = j[i]
+
+            if (i + 1) == len(j) and prevNum != None:
+                newState[l].append(prevNum)
+
+        for i in range(4):
+            while len(newState[l]) < 4:
+                newState[l].append(0)
+    return newState
 
 
 def move(state, direction):
-    if direction == 'up':
+        if direction == 'left':
+            return moveLeft(state)
+        elif direction == 'right':
+            return reverse(moveLeft(reverse(state)))
+        elif direction == 'up':
+            return transpose(moveLeft(transpose(state)))
+        elif direction == 'down':
+            return transpose(reverse(moveLeft(reverse(transpose(state)))))
 
-    for i in range(4):
-
-    pass
 
 
 def main():
@@ -27,10 +65,11 @@ def main():
 
 
 state = [
-    [2, 0, 0, 2],
-    [4, 16, 8, 2],
-    [2, 64, 32, 4],
+    [2, 2, 2, 2],
+    [4, 16, 4, 2],
+    [2, 64, 64, 4],
     [1024, 1024, 64, 0]
 ]
 
 printBoard(state)
+printBoard(move(state,'down'))
