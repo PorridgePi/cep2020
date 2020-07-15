1671958101fea40928fd2ab6ff991e5fa216c734
PorridgePi <www.kunbao2006@gmail.com>
2020-07-15 14:39:20 +0800

    optimised occurencev1 and finished occurencev2

--- occurrence-V2.py
+++ occurrence-V2.py
@@ -7,4 +7,6 @@ def occurrence(s1, s2):
     else:
         if s1[0:len(s2)] == s2:
             result += 1
-        return result + occurrence(s1[1:],s2)
+            return result + occurrence(s1[len(s2):],s2)
+        else:
+            return result + occurrence(s1[1:],s2)
