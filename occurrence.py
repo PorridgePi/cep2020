a57b993c08cebcaa5fdf896c8d83b9eb2341152a
PorridgePi <www.kunbao2006@gmail.com>
2020-07-15 14:21:47 +0800

    finished occurrence

--- occurrence.py
+++ occurrence.py
@@ -2,10 +2,9 @@ def occurrence(s1, s2):
     result = 0
     if s1 == s2:
         return 1
-    elif len(str(s2)) >= len(str(s1)):
+    elif len(s2) >= len(s1):
         return 0
     else:
-        if str(s1)[0:len(str(s2))] == s2:
+        if s1[0:len(str(s2))] == s2:
             result += 1
-        
-            
+        return result + occurrence(s1[1:],s2)
